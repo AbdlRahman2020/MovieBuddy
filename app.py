66dbac2f3b7ad3e10042099d8519c17aae93bf20
AbdlRahman Essa <abdlrahmanfathy2020@gmail.com>
2021-06-05 21:26:10 +0200

    update

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ app = flask.Flask(__name__, template_folder='templates')
 #reading the dataset
 df2 = pd.read_csv('./model/tmdb.csv')
 
-count = CountVectorizer(stop_words='english')
+
+count = CountVectorizer(stop_words='english') #
 count_matrix = count.fit_transform(df2['soup'])
 
 cosine_sim2 = cosine_similarity(count_matrix, count_matrix)
@@ -18,11 +19,12 @@ df2 = df2.reset_index()
 indices = pd.Series(df2.index, index=df2['title'])
 all_titles = [df2['title'][i] for i in range(len(df2['title']))]
 
+# Get recommendations function for the movie title
 def get_recommendations(title):
-    cosine_sim = cosine_similarity(count_matrix, count_matrix)
+    cosine_sim = cosine_similarity(count_matrix, count_matrix) # Using the Cosine Similarity function
     idx = indices[title]
-    sim_scores = list(enumerate(cosine_sim[idx]))
-    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+    sim_scores = list(enumerate(cosine_sim[idx])) # using the cosine function to list the movies
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True) # sorting the movies list
     sim_scores = sim_scores[1:11]
     movie_indices = [i[0] for i in sim_scores]
     tit = df2['title'].iloc[movie_indices]
@@ -35,20 +37,21 @@ def get_recommendations(title):
 # Set up the main route
 @app.route('/', methods=['GET', 'POST'])
 
+# main function for rendering the application
 def main():
     if flask.request.method == 'GET':
         return(flask.render_template('index.html'))
             
     if flask.request.method == 'POST':
-        m_name = flask.request.form['movie_name']
+        m_name = flask.request.form['movie_name'] # obtaining the user's input from the form
         m_name = m_name.title()
-#        check = difflib.get_close_matches(m_name,all_titles,cutout=0.50,n=1)
-        if m_name not in all_titles:
-            return(flask.render_template('negative.html',name=m_name))
+
+        if m_name not in all_titles: # Name doesn't match any movie in the dataset
+            return(flask.render_template('negative.html',name=m_name)) # render the negative.html page
         else:
-            result_final = get_recommendations(m_name)
-            names = []
-            dates = []
+            result_final = get_recommendations(m_name) # Get the recommendation for the movie name
+            names = []  # array to store the movies' names
+            dates = []  # array to store the movies' release dates
             for i in range(len(result_final)):
                 names.append(result_final.iloc[i][0])
                 dates.append(result_final.iloc[i][1])
